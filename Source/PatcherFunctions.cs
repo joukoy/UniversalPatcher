5963965e272f9d984a057fcdac51f0ae2a5b406e
Jouko Y <joukoy@gmail.com>
2020-02-11 13:58:32 +0200

    Source v 1.0

---
+++ Source/PatcherFunctions.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections.Generic;
+using System;
+using System.IO;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+public class upatcher
+{
+
+
+    public struct Patch
+    {
+        public string Name;
+        public string FileName;
+        public string Description;
+    }
+
+
+    public static string SelectFile(string Filter = "BIN files (*.bin)|*.bin|All files (*.*)|*.*")
+    {
+
+        OpenFileDialog fdlg = new OpenFileDialog();
+        fdlg.Title = "Select file";
+        fdlg.Filter = Filter;
+        fdlg.FilterIndex = 1;
+        fdlg.RestoreDirectory = true;
+        if (fdlg.ShowDialog() == DialogResult.OK)
+        {
+            return fdlg.FileName;
+        }
+        return "";
+
+    }
+    public static string SelectSaveFile(string Filter = "BIN files (*.bin)|*.bin")
+    {
+        SaveFileDialog saveFileDialog = new SaveFileDialog();
+        //saveFileDialog.Filter = "BIN files (*.bin)|*.bin";
+        saveFileDialog.Filter = Filter;
+        saveFileDialog.RestoreDirectory = true;
+        saveFileDialog.Title = "Save to file";
+
+        if (saveFileDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+        {
+            return saveFileDialog.FileName;
+        }
+        else
+            return "";
+
+    }
+
+
+    public static byte[] ReadBin(string FileName, uint FileOffset, uint Length)
+    {
+
+        byte[] buf = new byte[Length];
+
+        long offset = 0;
+        long remaining = Length;
+
+        using (BinaryReader freader = new BinaryReader(File.Open(FileName, FileMode.Open)))
+        {
+            freader.BaseStream.Seek(FileOffset, 0);
+            while (remaining > 0)
+            {
+                int read = freader.Read(buf, (int)offset, (int)remaining);
+                if (read <= 0)
+                    throw new EndOfStreamException
+                        (String.Format("End of stream reached with {0} bytes left to read", remaining));
+                remaining -= read;
+                offset += read;
+            }
+            freader.Close();
+        }
+        return buf;
+    }
+
+
+    public static void WriteSegmentToFile(string FileName, uint StartAddr, uint Length, byte[] Buf)
+    {
+
+        using (FileStream stream = new FileStream(FileName, FileMode.Create))
+        {
+            using (BinaryWriter writer = new BinaryWriter(stream))
+            {
+                writer.Write(Buf, (int)StartAddr, (int)Length);
+                writer.Close();
+            }
+        }
+
+    }
+
+
+
+
+}
---
+++ Source/frmPatcher.cs
@@ -0,0 +1,257 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using static upatcher;
+using System.Text.RegularExpressions;
+
+namespace UinversalPatcher
+{
+    public partial class FrmPatcher : Form
+    {
+        public FrmPatcher()
+        {
+            InitializeComponent();
+        }
+
+        private static List<uint> PatchData;
+        private static List<uint> PatchAddr;
+        byte[] buf;
+
+        private void FrmPatcher_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void btnOrgFile_Click(object sender, EventArgs e)
+        {
+            string BinFile = SelectFile();
+            if (BinFile.Length > 1)
+            {
+                txtBaseFile.Text = BinFile;
+            }
+        }
+
+        private void btnModFile_Click(object sender, EventArgs e)
+        {
+            string BinFile;
+            if (radioCreate.Checked)
+                BinFile = SelectFile();
+            else
+                BinFile = SelectFile("PACTH files (*.patch)|*.patch|ALL files(*.*) | *.*");
+            if (BinFile.Length > 1)
+            {
+                txtModifierFile.Text = BinFile;
+            }
+
+        }
+
+        private bool ApplyPatch()
+        {
+            string line;
+
+            try { 
+                StreamReader sr = new StreamReader(txtModifierFile.Text);
+                bool FirstLine = true;
+                long fsize = new System.IO.FileInfo(txtBaseFile.Text).Length;
+                labelBinSize.Text = fsize.ToString();
+                buf = ReadBin(txtBaseFile.Text, 0, (uint)fsize);
+                while ((line = sr.ReadLine()) != null)
+                {
+                    if (FirstLine)
+                    {
+                        if (line != fsize.ToString())
+                        {
+                            Logger("File size doesn't match patch");
+                            return false;
+                        }
+                    }
+                    FirstLine = false;
+
+                    string tmp = Regex.Replace(line, "[^0-9:]", "");
+                    if (tmp != line)
+                        Logger("(" + line + ")");
+                    else
+                    {
+                        string[] LineParts = line.Split(':');
+                        if (LineParts.Length >1) { 
+                            uint Addr = uint.Parse(LineParts[0]);
+                            uint Data = uint.Parse(LineParts[1]);
+                            if (Addr > buf.Length)
+                                throw new FileLoadException(String.Format("Address {0} out of range!",  Addr.ToString("X4")));
+                            if (Data > 0xff)
+                                throw new FileLoadException(String.Format("Data {0} out of range!", Data.ToString("X4")));
+                            //Apply patchrow:
+                            Logger("Set address: ".PadRight(16) + Addr.ToString("X4").PadRight(10) + "Data:   " + Data.ToString("X4"));
+                            buf[Addr] = byte.Parse(LineParts[1]);
+                        }
+                    }
+                }
+                sr.Close();
+            }
+            catch (Exception ex)
+            {
+                Logger("Error: " + ex.Message);
+                return false;
+            }
+            Logger("[OK]");
+            return true;
+        }
+
+        public void CompareBins()
+        {
+            try
+            {
+                long fsize = new System.IO.FileInfo(txtBaseFile.Text).Length;
+                long fsize2 = new System.IO.FileInfo(txtModifierFile.Text).Length;
+                if (fsize != fsize2)
+                {
+                    MessageBox.Show("Files are different size, will not compare!");
+                    return;
+                }
+
+                byte[] OrgFile = ReadBin(txtBaseFile.Text, 0, (uint)fsize);
+                byte[] ModFile = ReadBin(txtModifierFile.Text, 0, (uint)fsize);
+                labelBinSize.Text = fsize.ToString();
+                
+                for (uint i = 0; i < fsize; i++)
+                {
+                    if (OrgFile[i] != ModFile[i])
+                    {
+                        PatchAddr.Add(i);
+                        PatchData.Add(ModFile[i]);
+                        txtResult.AppendText(i.ToString("X1") + ":" + OrgFile[i].ToString("X1") + "=>" + ModFile[i].ToString("X1") + Environment.NewLine);
+                    }
+
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Logger("Error: " + ex.Message);
+            }
+        }
+         private void btnCompare_Click(object sender, EventArgs e)
+        {
+            PatchData = new List<uint>();
+            PatchAddr = new List<uint>();
+            txtResult.Text = "";
+            if (radioCreate.Checked) { 
+                CompareBins();
+                if (PatchAddr.Count>0)
+                {
+                    btnSave.Enabled = true;
+                    btnSave.Text = "Save patch";
+                    txtPatchName.Enabled = true;
+                }
+                else
+                {
+                    btnSave.Enabled = false;
+                    txtPatchName.Enabled = false;
+                }
+            }
+            else //Apply patch selected
+            {
+                if (ApplyPatch())
+                {
+                    btnSave.Enabled = true;
+                    txtPatchName.Enabled = true;
+                }
+                else
+                {
+                    btnSave.Enabled = false;
+                    txtPatchName.Enabled = false;
+                }
+            }
+
+        }
+
+        private void SavePatch()
+        {
+            try
+            {
+                if (txtPatchName.Text.Length < 1)
+                {
+                    Logger("Supply patch description");
+                    return;
+                }
+                string PatchName = labelBinSize.Text + "-" + txtPatchName.Text;
+                string PatchFile = SelectSaveFile("PACTH files (*.patch)|*.patch|ALL files(*.*) | *.*");
+                Logger("Saving to file: " + PatchFile);
+                StreamWriter sw = new StreamWriter(PatchFile);
+                sw.WriteLine(labelBinSize.Text);
+                sw.WriteLine(txtPatchName.Text);
+                for (int i = 0; i < PatchAddr.Count; i++)
+                {
+                    sw.WriteLine(PatchAddr[i].ToString() + ":" + PatchData[i].ToString());
+                }
+                sw.Close();
+                Logger("Patch saved");
+            }
+            catch (Exception ex)
+            {
+                Logger("Error: " + ex.Message);
+            }
+
+        }
+
+        private void SaveBin()
+        {
+            try
+            {
+            string BinFile = SelectSaveFile();
+            Logger("Saving to file: " + BinFile);
+            WriteSegmentToFile(BinFile, 0, (uint)buf.Length, buf);
+            }
+            catch (Exception ex)
+            {
+                Logger("Error: " + ex.Message);
+            }
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            if (radioCreate.Checked)
+                SavePatch();
+            else
+                SaveBin();
+        }
+
+        public void Logger(string LogText, Boolean NewLine = true)
+        {
+            txtResult.AppendText(LogText);
+            if (NewLine)
+                txtResult.AppendText(Environment.NewLine);
+        }
+        private void txtModifierFile_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioCreate_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioCreate.Checked)
+            {
+                btnOrgFile.Text = "Original file";
+                btnModFile.Text = "Modified file";
+                btnCompare.Text = "Compare";
+                btnSave.Text = "Save Patch";
+                txtPatchName.Visible = true;
+                labelDescr.Visible = true;
+            }
+            else
+            {
+                btnOrgFile.Text = "BIN file";
+                btnModFile.Text = "Patch File";
+                btnCompare.Text = "Apply Patch";
+                btnSave.Text = "Save BIN";
+                txtPatchName.Visible = false;
+                labelDescr.Visible = false;
+            }
+        }
+    }
+}
