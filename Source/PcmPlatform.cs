2fe48767e0ce4c64c7d7192afd1ae5df546befc3
Jouko Y <joukoy@gmail.com>
2021-09-30 08:09:18 +0300

    0.18.16

---
+++ Source/PcmPlatform.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace UniversalPatcher
+{
+    public class PcmPlatform
+    {
+        public PcmPlatform()
+        {
+            MSB = true;
+        }
+        public bool MSB { get; set; }
+        public string SegmentFile { get; set; }
+        public string TableSeekFile { get; set; }
+        public string SegmentSeekFile { get; set; }
+        public string PidSearchString { get; set; }
+        public uint PidSearchStep { get; set; }
+    }
+}
