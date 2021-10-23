93a4974fe706c1f95ed2fe6fb2d058418cc1b2a8
Jouko Y <joukoy@gmail.com>
2021-10-23 21:17:50 +0300

    0.18.43

---
+++ Source/frmImportFile.Designer.cs
@@ -0,0 +1,109 @@
+﻿
+namespace UniversalPatcher
+{
+    partial class frmImportFile
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtOffset = new System.Windows.Forms.TextBox();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.txtResult = new System.Windows.Forms.RichTextBox();
+            this.labelFileName = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(10, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Offset (HEX):";
+            // 
+            // txtOffset
+            // 
+            this.txtOffset.Location = new System.Drawing.Point(85, 6);
+            this.txtOffset.Name = "txtOffset";
+            this.txtOffset.Size = new System.Drawing.Size(100, 20);
+            this.txtOffset.TabIndex = 2;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(191, 4);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 4;
+            this.btnSave.Text = "Save bin";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // txtResult
+            // 
+            this.txtResult.Font = new System.Drawing.Font("Consolas", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtResult.HideSelection = false;
+            this.txtResult.Location = new System.Drawing.Point(1, 32);
+            this.txtResult.Name = "txtResult";
+            this.txtResult.Size = new System.Drawing.Size(652, 106);
+            this.txtResult.TabIndex = 5;
+            this.txtResult.Text = "";
+            // 
+            // labelFileName
+            // 
+            this.labelFileName.AutoSize = true;
+            this.labelFileName.Location = new System.Drawing.Point(272, 9);
+            this.labelFileName.Name = "labelFileName";
+            this.labelFileName.Size = new System.Drawing.Size(10, 13);
+            this.labelFileName.TabIndex = 6;
+            this.labelFileName.Text = "-";
+            // 
+            // frmImportFile
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(653, 138);
+            this.Controls.Add(this.labelFileName);
+            this.Controls.Add(this.txtResult);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.txtOffset);
+            this.Controls.Add(this.label1);
+            this.Name = "frmImportFile";
+            this.Text = "Import";
+            this.Load += new System.EventHandler(this.frmImportFile_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtOffset;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.RichTextBox txtResult;
+        private System.Windows.Forms.Label labelFileName;
+    }
+}
---
+++ Source/frmImportFile.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using static upatcher;
+
+namespace UniversalPatcher
+{
+    public partial class frmImportFile : Form
+    {
+        public frmImportFile()
+        {
+            InitializeComponent();
+        }
+
+        public string outFileName = "";
+
+        private void frmImportFile_Load(object sender, EventArgs e)
+        {
+            LogReceivers.Add(txtResult);
+        }
+        public void LoggerBold(string LogText, Boolean NewLine = true)
+        {
+            txtResult.SelectionFont = new Font(txtResult.Font, FontStyle.Bold);
+            txtResult.AppendText(LogText);
+            txtResult.SelectionFont = new Font(txtResult.Font, FontStyle.Regular);
+            if (NewLine)
+                txtResult.AppendText(Environment.NewLine);
+        }
+
+        public void Logger(string LogText, Boolean NewLine = true)
+        {
+            txtResult.AppendText(LogText);
+            if (NewLine)
+                txtResult.AppendText(Environment.NewLine);
+            Application.DoEvents();
+        }
+
+        public void importMotorola(string fileName)
+        {
+            try
+            {
+                this.Text = "Import Motorola S-Record";
+                labelFileName.Text = fileName;
+                SRecord srecord = new SRecord();
+                StreamReader file = new StreamReader(labelFileName.Text);
+                SRecordStructure sRec = srecord.Read(file); //Skip first block. Header ?
+                Logger("Starting address:" + sRec.address.ToString("X") + "(HEX), " + sRec.address.ToString() + " (DEC)");
+                txtOffset.Text = sRec.address.ToString("X");
+                file.Close();
+            }
+            catch (Exception ex)
+            {
+                LoggerBold(ex.Message);
+            }
+
+        }
+
+        public void importIntel(string fileName)
+        {
+            try
+            {
+                this.Text = "Import Intel HEX";
+                labelFileName.Text = fileName;
+                IntelHex intelHex = new IntelHex();
+                StreamReader file = new StreamReader(labelFileName.Text);
+                //IntelHexStructure ihex = intelHex.Read(file); //Skip first block. Header ?
+                while (true)
+                {
+                    IntelHexStructure ihex = intelHex.Read(file);
+                    if (ihex == null)
+                        break;
+                    if (ihex.type == 4)
+                    {
+                        Logger("Segment address:" + ihex.address.ToString("X") + " (HEX), " + ihex.address.ToString() + " (DEC)");
+                    }
+                    if (ihex.type == 0) //First datablock
+                    {
+                        Logger("Starting address:" + ihex.address.ToString("X") + " (HEX), " + ihex.address.ToString() + " (DEC)");
+                        txtOffset.Text = ihex.address.ToString("X");
+                        break;
+                    }
+
+                }
+                file.Close();
+
+
+            }
+            catch (Exception ex)
+            {
+                LoggerBold(ex.Message);
+            }
+
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string defName = Path.GetFileNameWithoutExtension(labelFileName.Text) + ".bin";
+                outFileName = SelectSaveFile("BIN (*.bin)|*.bin|All(*.*)|*.*", defName);
+                if (outFileName.Length == 0)
+                    return;
+                Logger("Saving to file: " + outFileName, false);
+                FileStream stream = new FileStream(outFileName, FileMode.Create);
+                BinaryWriter writer = new BinaryWriter(stream);
+                uint offset = 0;
+                if (!HexToUint(txtOffset.Text, out offset))
+                {
+                    LoggerBold("Can't decode HEX offset: " + txtOffset.Text);
+                    return;
+                }
+                if (this.Text.Contains("Motorola"))
+                {
+                    StreamReader file = new StreamReader(labelFileName.Text);
+                    SRecord srecord = new SRecord();
+                    while (true)
+                    {
+                        SRecordStructure sRec = srecord.Read(file);
+                        if (sRec == null)
+                            break;
+                        if (sRec.type > 0 && sRec.type < 4) //Data records are type  1 - 3
+                        {
+                            writer.Seek((int)(sRec.address - offset), SeekOrigin.Begin);
+                            writer.Write(sRec.data, 0, sRec.dataLen);
+                        }
+                    }
+                    file.Close();
+                
+                }
+                else
+                {
+                    IntelHex intelHex = new IntelHex();
+                    StreamReader file = new StreamReader(labelFileName.Text);
+                    //IntelHexStructure ihex = intelHex.Read(file); //Skip first block. Header ?
+                    while (true)
+                    {
+                        IntelHexStructure ihex = intelHex.Read(file);
+                        if (ihex == null)
+                            break;
+                        if (ihex.type == 0) //Datablock
+                        {
+                            writer.Seek((int)(ihex.address - offset), SeekOrigin.Begin);
+                            writer.Write(ihex.data, 0, ihex.dataLen);
+                        }
+                    }
+
+                }
+                writer.Close();
+                Logger(" [OK]");
+                this.DialogResult = DialogResult.OK;
+                this.Close();
+            }
+            catch (Exception ex)
+            {
+                LoggerBold(ex.Message);
+            }
+        }
+
+
+    }
+}
