3bcd3af65675846971ad543f35dc004665036d0c
JoukoY <joukoy@gmail.com>
2022-01-28 13:32:01 +0200

    0.20.4

---
+++ Source/J2534DotNet/J2534Device.cs
@@ -0,0 +1,107 @@
+﻿#region Copyright (c) 2010, Michael Kelly
+/* 
+ * Copyright (c) 2010, Michael Kelly
+ * michael.e.kelly@gmail.com
+ * http://michael-kelly.com/
+ * 
+ * All rights reserved.
+ * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+ * Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+ * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+ * Neither the name of the organization nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * 
+ */
+#endregion License
+namespace J2534DotNet
+{
+    public class J2534Device
+    {
+        public string Vendor { get; set; }
+        public string Name { get; set; }
+        public string FunctionLibrary { get; set; }
+        public string ConfigApplication { get; set; }
+        public int CAN { get; set; }
+        public int ISO15765 { get; set; }
+        public int J1850PWM { get; set; }
+        public int J1850VPW { get; set; }
+        public int ISO9141 { get; set; }
+        public int ISO14230 { get; set; }
+        public int SCI_A_ENGINE { get; set; }
+        public int SCI_A_TRANS { get; set; }
+        public int SCI_B_ENGINE { get; set; }
+        public int SCI_B_TRANS { get; set; }
+        public int DiCECompatible { get; set; }
+
+        public bool IsCANSupported
+        {
+            get { return (CAN > 0 ? true : false); }
+        }
+
+        public bool IsISO15765Supported
+        {
+            get { return (ISO15765 > 0 ? true : false); }
+        }
+
+        public bool IsJ1850PWMSupported
+        {
+            get { return (J1850PWM > 0 ? true : false); }
+        }
+
+        public bool IsJ1850VPWSupported
+        {
+            get { return (J1850VPW > 0 ? true : false); }
+        }
+
+        public bool IsISO9141Supported
+        {
+            get { return (ISO9141 > 0 ? true : false); }
+        }
+
+        public bool IsISO14230Supported
+        {
+            get { return (ISO14230 > 0 ? true : false); }
+        }
+
+        public bool IsSCI_A_ENGINESupported
+        {
+            get { return (SCI_A_ENGINE > 0 ? true : false); }
+        }
+
+        public bool IsSCI_A_TRANSSupported
+        {
+            get { return (SCI_A_TRANS > 0 ? true : false); }
+        }
+
+        public bool IsSCI_B_ENGINESupported
+        {
+            get { return (SCI_B_ENGINE > 0 ? true : false); }
+        }
+
+        public bool IsSCI_B_TRANSSupported
+        {
+            get { return (SCI_B_TRANS > 0 ? true : false); }
+        }
+
+        public bool IsDiCECompatible
+        {
+            get { return (DiCECompatible > 0 ? true : false);  }
+        }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
