c783fb7f5290eae56be4ffec2e5bb58157618981
JoukoY <joukoy@gmail.com>
2022-02-24 12:03:28 +0200

    0.20.9

---
+++ Source/frmCredits.Designer.cs
@@ -0,0 +1,61 @@
+﻿
+namespace UniversalPatcher
+{
+    partial class frmCredits
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richCredits = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // richCredits
+            // 
+            this.richCredits.Location = new System.Drawing.Point(96, 59);
+            this.richCredits.Name = "richCredits";
+            this.richCredits.ReadOnly = true;
+            this.richCredits.Size = new System.Drawing.Size(408, 468);
+            this.richCredits.TabIndex = 0;
+            this.richCredits.Text = "";
+            // 
+            // Credits
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(600, 715);
+            this.Controls.Add(this.richCredits);
+            this.Name = "Credits";
+            this.Text = "Credits";
+            this.Load += new System.EventHandler(this.Credits_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richCredits;
+    }
+}
---
+++ Source/frmCredits.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using static Upatcher;
+namespace UniversalPatcher
+{
+    public partial class frmCredits : Form
+    {
+        public frmCredits()
+        {
+            InitializeComponent();
+        }
+
+        private void Credits_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                richCredits.Dock = DockStyle.Fill;
+                richCredits.LoadFile("Credits.rtf");
+            }
+            catch (Exception ex)
+            {
+                LoggerBold(ex.Message);
+                this.Dispose();
+            }
+        }
+    }
+}
